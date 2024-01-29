6d768a65178e7cded9f4aa82bb9e87b4971e1aad
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2024-01-29 11:54:02 +0100

    /

--- PraticaClassi/Program.cs
+++ PraticaClassi/Program.cs
@@ -1,65 +1,69 @@
 ﻿using PraticaClassi.Models;
+using System.Collections.Generic;
 
 namespace PraticaClassi
 {
     internal class Program
     {
         static void ShowPerson(Person person)
-        {          
+        {
             Console.WriteLine("First name: " + person.FirstName + "\nLast name: " + person.LastName + "\nAge: " + person.Age + "\nJob: " + person.Job);
         }
 
+        static Person CreatePerson()
+        {
+            Console.WriteLine("Insert your name");
+            string nameInput = Console.ReadLine()!.Trim();
+            Console.WriteLine("Insert your last name");
+            string lastNameInput = Console.ReadLine()!.Trim();
+            Console.WriteLine("Insert your occupation");
+            string occupationInput = Console.ReadLine()!.Trim();
+            Console.WriteLine("Insert your age");
+            string ageInput = Console.ReadLine()!.Trim();
+            Person createdPerson = new Person();
+            createdPerson.FirstName = nameInput;
+            createdPerson.LastName = lastNameInput;
+            createdPerson.Job = occupationInput;
+            int parsedAge;
+            bool parsableAge = int.TryParse(ageInput, out parsedAge);
+            if (parsableAge)
+            {
+                createdPerson.Age = parsedAge;
+
+            } else
+            {
+                Console.WriteLine("Please insert a valid age");
+               
+            }
+            return createdPerson;
+        }
         static void Main(string[] args)
         {
-            Person steveStevenson = new Person();
-            steveStevenson.FirstName = "Steve";
-            steveStevenson.LastName = "Stevenson";
-            steveStevenson.Age = 32;
-            steveStevenson.Job = "Programmer";
+            List<Person> people = new List<Person>();
+            Console.WriteLine("How many people do you want to add?");
+            //string numberOfPeople = Console.ReadLine()!;
+            //int convertedNumber = Convert.ToInt32(numberOfPeople);
+            for (int i = 0; i < 3; i++)
+            {
+                people.Add(CreatePerson());
 
-            Person johnJohnson = new Person();
-            johnJohnson.FirstName = "John";
-            johnJohnson.LastName = "Johnson";
-            johnJohnson.Age = 24;
-            johnJohnson.Job = "Professional thief";
-
-            Person mariannaRossi = new Person();
-            mariannaRossi.FirstName = "Marianna";
-            mariannaRossi.LastName = "Rossi";
-            mariannaRossi.Age = 19;
-            mariannaRossi.Job = "Serial killer";
-
-            Person ermannoErmannini = new Person();
-            ermannoErmannini.FirstName = "Ermanno";
-            ermannoErmannini.LastName = "Ermannini";
-            ermannoErmannini.Age = 56;
-            ermannoErmannini.Job = "God";
-
-            Person andrewMowlinars = new Person();
-            andrewMowlinars.FirstName = "Andrew";
-            andrewMowlinars.LastName = "Mowlinars";
-            andrewMowlinars.Age = 29;
-            andrewMowlinars.Job = "Cutscene watcher";
-
-            Person[] people = [ steveStevenson, andrewMowlinars, ermannoErmannini, mariannaRossi, johnJohnson ];
-
+            }
             Console.WriteLine("Insert the last name of the person you want to doxx");
             string input = Console.ReadLine()!;
             Person foundPerson = null;
-            for (int i = 0; i < people.Length; i++)
+            foreach (var person in people)
             {
-                if (people[i].LastName.ToLower() == input.ToLower()) {
-                    foundPerson = people[i]; 
+                if (person.LastName.ToLower() == input.ToLower())
+                {
+                    foundPerson = person;
                     ShowPerson(foundPerson);
                     break;
-                } 
-                    
                 };
+            }
             if (foundPerson == null)
             {
                 Console.WriteLine("No such person exists");
             }
-            
         }
     }
 }
